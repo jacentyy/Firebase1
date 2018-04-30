77798b34945e8bc81dc3195db185098e2a8d2d97
jacentyy <polexi.polexi@gmail.com>
2018-04-30 17:35:54 +0200

    Initial commit

---
+++ src/main/java/info/thingsy/thingsy/InsertActivity.java
@@ -0,0 +1,13 @@
+package info.thingsy.thingsy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+    }
+}
