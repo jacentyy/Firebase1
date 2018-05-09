96688f10de1ee5b6ff8fcd0842bb162721023338
jacentyy <polexi.polexi@gmail.com>
2018-05-09 20:12:51 +0200

    Managing Authentication begginings

--- src/main/java/info/thingsy/thingsy/FirebaseUtil.java
+++ src/main/java/info/thingsy/thingsy/FirebaseUtil.java
@@ -1,5 +1,8 @@
 package info.thingsy.thingsy;
 
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,6 +16,8 @@ public class FirebaseUtil {
     public static FirebaseDatabase mFirebaseDatabase;
     public static DatabaseReference mDatabaseReference;
     private static FirebaseUtil firebaseUtil;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthListener;
     public static ArrayList<TravelDeal> mDeals;
 
     private FirebaseUtil() {}
@@ -21,8 +26,22 @@ public class FirebaseUtil {
         if (firebaseUtil == null) {
             firebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = FirebaseDatabase.getInstance();
+            mFirebaseAuth = FirebaseAuth.getInstance();
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                }
+            };
         }
         mDeals = new ArrayList<TravelDeal>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    public static void attachListener() {
+        mFirebaseAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public static void detachListener() {
+        mFirebaseAuth.removeAuthStateListener(mAuthListener);
     }
 }
